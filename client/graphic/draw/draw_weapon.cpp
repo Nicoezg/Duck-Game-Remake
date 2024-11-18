eb9c390b8a41418db7206c79c1c0dde1a1ae2bbc
Nicoezg <ngruner@fi.uba.ar>
2024-11-18 16:38:12 -0300

    feat: hay mapa, agrego el mapa forest

--- client/graphic/draw/draw_weapon.cpp
+++ client/graphic/draw/draw_weapon.cpp
@@ -27,7 +27,7 @@ DrawWeapon::DrawWeapon(SDL2pp::Renderer &renderer, WeaponId weaponId) : textures
 }
 
 void DrawWeapon::render(int x, int y, int flipType){
-    std::shared_ptr<SDL2pp::Texture> texture = this->textures[4];
+    std::shared_ptr<SDL2pp::Texture> texture = this->textures[2];
     SDL2pp::Rect dest(x + 3, y + 9, 25, 25);
     renderer.Copy(*texture, SDL2pp::NullOpt, dest, 0.0, SDL2pp::NullOpt, flipType);
     if (shoot){
