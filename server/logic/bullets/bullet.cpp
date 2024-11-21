b43f971fab228fc039bef93c3c4e899864a87271
Franco Daniel Capra <fcapra@fi.uba.ar>
2024-11-21 17:08:16 -0300

    refactor: Cambio las configuraciones

--- server/logic/bullets/bullet.cpp
+++ server/logic/bullets/bullet.cpp
@@ -4,41 +4,38 @@
 
 const double PI = 3.14159265358979323846;
 const int TILE_SIZE = 16;
+const int SPEED = CONFIG.getBulletSpeed();
+
 
 Bullet::Bullet(int owner_Id, int pos_x, int pos_y, float angle, bool canBounce,
                int range, BulletId id, bool isRight, bool upwards)
-    : owner_Id(owner_Id), pos_x(pos_x), pos_y(pos_y), angle(angle),
-      range(range), canBounce(canBounce), id(id), isRight(isRight), upwards(upwards), traveledDistance(0) {}
+        : owner_Id(owner_Id), pos_x(pos_x), pos_y(pos_y), angle(angle),
+          range(range), canBounce(canBounce), id(id), isRight(isRight), upwards(upwards), traveledDistance(0) {}
 
 void Bullet::update() {
-  double radianAngle = angle * (PI / 180.0);
+    double radianAngle = angle * (PI / 180.0);
 
-  int deltaX = static_cast<int>(CONFIG.getBulletSpeed() * std::cos(radianAngle));
-  int deltaY = static_cast<int>(10 * std::sin(radianAngle));
+    int deltaX = static_cast<int>((isRight ? 1:-1) * SPEED * std::cos(radianAngle));
+    int deltaY = static_cast<int>(10 * std::sin(radianAngle));
 
-  if (upwards) {
-    pos_y -= CONFIG.getBulletSpeed();
-  } else {
-    if (isRight) {
-      pos_x += deltaX;
-      pos_y += deltaY;
+    if (upwards) {
+        pos_y -= SPEED;
     } else {
-      pos_x -= deltaX;
-      pos_y += deltaY;
+        pos_x += deltaX;
+        pos_y += deltaY;
     }
-  }
 
-  int distanceMoved = std::sqrt(deltaX * deltaX + deltaY * deltaY);
-  traveledDistance += distanceMoved;
+    int distanceMoved = std::sqrt(deltaX * deltaX + deltaY * deltaY);
+    traveledDistance += distanceMoved;
 
 }
 
 bool Bullet::outOfRange() {
-  int tilesMoved = traveledDistance / TILE_SIZE;
-  if (tilesMoved >= range) {
-    return true;
-  }
-  return false;
+    int tilesMoved = traveledDistance / TILE_SIZE;
+    if (tilesMoved >= range) {
+        return true;
+    }
+    return false;
 }
 
 void Bullet::bounce(int newAngle) { angle = newAngle; }
@@ -52,6 +49,6 @@ float Bullet::getAngle() const { return angle; }
 BulletId Bullet::getId() const { return id; }
 
 BulletDTO Bullet::toDTO() const {
-  return {pos_x, pos_y, id, angle};
+    return {pos_x, pos_y, id, angle};
 }
 
