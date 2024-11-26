131d226548afdcb2436a34cd583803489efcd67e
BautistaBoeri <bboeri@fi.uba.ar>
2024-11-26 11:32:21 -0300

    fix:granada explota solo una vez

--- server/logic/throwables/grenade.h
+++ server/logic/throwables/grenade.h
@@ -7,15 +7,14 @@ class ThrownGrenade : public Throwable {
 private:      
   int framesToExplode; 
   bool ownerHasGrenade;
+  float angle;
 
 public:
-  ThrownGrenade(GameMap &map,bool isRight,int pos_x,int pos_y,int framesToexplode):Throwable(map,isRight,pos_x,pos_y,5) , framesToExplode(framesToexplode) {
+  ThrownGrenade(GameMap &map,bool isRight,int pos_x,int pos_y,int framesToexplode):Throwable(map,isRight,pos_x,pos_y,5) , framesToExplode(framesToexplode),angle(0) {
   }
   void update() override;
 
-  BulletDTO toDTO() const override {
-    return {pos_x, pos_y, THROWN_GRENADE, 0, isRight};
-  }
+  BulletDTO toDTO() const override;
 
   bool isOver() const override { return framesToExplode == 0; }
   
