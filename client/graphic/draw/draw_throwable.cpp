1a2a84b256d860da2993896d4f5e9084bed636ea
Nicoezg <ngruner@fi.uba.ar>
2024-11-27 16:23:51 -0300

    fix: grenade rota desde el centro

--- client/graphic/draw/draw_throwable.cpp
+++ client/graphic/draw/draw_throwable.cpp
@@ -4,7 +4,7 @@
 
 DrawThrowable::DrawThrowable(SDL2pp::Renderer &renderer) : renderer(renderer), sound() {
   this->textures[0] = std::make_shared<SDL2pp::Texture>(
-      renderer, SDL2pp::Surface(DATA_PATH "weapons/grenade.png"));
+      renderer, SDL2pp::Surface(DATA_PATH "weapons/grenade-test.png"));
   this->textures[1] = std::make_shared<SDL2pp::Texture>(
       renderer, SDL2pp::Surface(DATA_PATH "weapons/banana-placed.png"));
   this->textures[2] = std::make_shared<SDL2pp::Texture>(
@@ -16,13 +16,15 @@ DrawThrowable::DrawThrowable(SDL2pp::Renderer &renderer) : renderer(renderer), s
 
 void DrawThrowable::render(const ThrowableDTO &throwable) {
   std::shared_ptr<SDL2pp::Texture> texture = this->textures[throwable.get_id()];
+  int width = texture->GetWidth();
+  int height = texture->GetHeight();
   SDL2pp::Rect dest(throwable.get_position_x(), throwable.get_position_y(),
-                    texture->GetWidth(), texture->GetHeight());
+                    width, height);
 
   int direction = throwable.get_direction();
 
   renderer.Copy(*texture, SDL2pp::NullOpt, dest, throwable.get_angle(),
-                SDL2pp::Point(), direction ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
+                SDL2pp::Point(width / 2, height / 2), direction ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
   if (throwable.get_action()) {
     sound.change(this->sounds[throwable.get_id()]);
     sound.play();
